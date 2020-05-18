af75ced1b0a44ef457bdedeb7891efedb81be491
Brennon Nuckols <brennuck@gmail.com>
2020-05-17 22:58:16 -0600

    patch

--- index.js
+++ index.js
@@ -33,30 +33,48 @@ server.get("/api/users/:id", (req, res) => {
 });
 
 server.post("/api/users", (req, res) => {
-    const userInfo = req.body;
+  const userInfo = req.body;
 
-    if (!userInfo.name || !userInfo.bio) {
-        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
-    } else if (userInfo.name || userInfo.bio) {
-        users.push(userInfo);
-        res.status(201).json(users)
-    } else {
-        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
-    }
-})
+  if (!userInfo.name || !userInfo.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else if (userInfo.name || userInfo.bio) {
+    users.push(userInfo);
+    res.status(201).json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
+  }
+});
 
-server.delete('/api/users/:id', (req, res) => {
-    const {id} = req.params;
-  
-    const found = users.find(user => user.id === id);
-  
-    if (found) {
-      users = users.filter(user => user.id !== id);
-      res.status(200).json(found);
-    } else {
-      res.status(404).json({message: "not found"});
-    }
-  });
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const found = users.find((user) => user.id === id);
+
+  if (found) {
+    users = users.filter((user) => user.id !== id);
+    res.status(200).json(found);
+  } else {
+    res.status(404).json({ message: "not found" });
+  }
+});
+
+server.patch("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  let found = users.find((use) => use.id === id);
+
+  if (found) {
+    Object.assign(found, changes);
+    res.status(200).json(found);
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+});
 
 const port = 5000;
 server.listen(port, () =>
